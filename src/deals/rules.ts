e17700fe9a88768e6368f0f768f25bb3fb2c4ef6
Jawad Nazir <Jawad.nazir124@gmail.com>
2025-07-20 19:30:42 +0100

    Rules for Triggers and Offers

---
+++ src/deals/rules.ts
@@ -0,0 +1,14 @@
+enum OfferRule {
+  PERCENTAGE_DISCOUNT = "PercentageDiscount",
+  FIXED_DISCOUNT = "FixedDiscount",
+  FREE_ITEM = "FreeItem"
+}
+
+enum TriggerRule {
+  ITEM = "Item",
+  BUY_X_QUANTITY = "BuyXQuantity",
+  BUY_MORE_THAN_X = "BuyMoreThanX"
+}
+
+
+export { OfferRule, TriggerRule };
