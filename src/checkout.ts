7bd522208888f418d9482fefd3609193dc4af443
Jawad Nazir <Jawad.nazir124@gmail.com>
2025-07-20 11:27:47 +0100

    Generate receipt initial work started.  Receipt items are generated with total price. Need to add special offers now.

--- src/checkout.ts
+++ src/checkout.ts
@@ -1,13 +1,84 @@
 import { Product } from './product';
-import { Receipt } from './receipt';
+import { Receipt, ReceiptItem } from './receipt';
+
 
 export class Checkout {
+    private products : Array<Product> = [];
 
     public scanItem(product: Product): void {
-        console.log('todo')
+        this.products.push(product);
     }
 
     public generateReceipt(): Receipt {
-        return new Receipt();
+        // Receipt item
+        // Product - name & price + quantity.
+        const receiptItems = this.products.reduce(
+            (
+                productsSoFar: ReceiptItem[],
+                product,
+            ) => {
+                const existing = productsSoFar.find(psf => psf.product.name == product.name);
+
+                if(existing) {
+                   existing.quantity++
+                   return; 
+                } 
+
+                productsSoFar.push({ product, quantity: 1 });
+
+                return productsSoFar;
+            }, []
+        );
+
+        const totalPrice = receiptItems.reduce((acc, item) => acc + item.product.price * item.quantity, 0) 
+
+        // Apply special offers?
+
+        // Special offer
+        // CallBack
+        return new Receipt(receiptItems, totalPrice);
     }
 }
+
+
+
+
+
+
+const specialOffersKeys = {
+    PERCENTAGE_DISCOUNT: "PercentageDiscount",
+    FIXED_DISCOUNT: "FixedDiscount",
+    FREE_ITEM: "FreeItem",
+}
+
+const triggers = {
+    DEFAULT: "default",
+    BUY_X_QUANTITY: "BuyXQuantity"
+}
+
+interface SpecialOffer {
+    offerKey: string,
+    trigger: string,
+}
+
+
+const specialOffers: SpecialOffer[] = [
+    {
+        offerKey: specialOffersKeys.PERCENTAGE_DISCOUNT,
+        trigger:triggers.BUY_X_QUANTITY,
+
+    },
+]
+    // Separate trigger from offer
+    // Combine the two to create a deal!
+
+    // OFFER
+        // Discount
+            // Percentage
+            // Fixed discount 
+        // Free item
+
+    // TRIGGERS
+        // Item itself
+        // Buy X quantity
+
