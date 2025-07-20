6da4c2eee04e213c506494113720751ed8ec5d5f
Jawad Nazir <Jawad.nazir124@gmail.com>
2025-07-20 11:27:15 +0100

    Assign fields in ctor

--- src/receipt.ts
+++ src/receipt.ts
@@ -8,4 +8,9 @@ export interface ReceiptItem {
 export class Receipt {
     public readonly items: Array<ReceiptItem> = [];
     public readonly totalPrice: number = 0;
+
+    constructor(items: Array<ReceiptItem>, totalPrice: number){
+        this.items = items;
+        this.totalPrice = totalPrice;
+    }
 }
